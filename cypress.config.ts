c54e78a8e8ee43123750b3cf24b46708f8522d8d
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2024-05-13 13:58:36 +0530

    Init - test parallelization

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
+const cypressSplit = require('cypress-split')
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      cypressSplit(on, config)
+      return config
     },
   },
 });
